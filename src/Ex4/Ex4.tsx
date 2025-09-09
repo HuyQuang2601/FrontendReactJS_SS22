d7b1f358bada362e4d42d233aa29a8842ba9f816
HuyQuang2601 <huyxauzai2601@gmail.com>
2025-09-09 09:52:23 +0700

    Update Ex4.tsx

--- src/Ex4/Ex4.tsx
+++ src/Ex4/Ex4.tsx
@@ -1,34 +1,18 @@
 
-import { Card, Button } from "react-bootstrap";
-import mac1 from "../assets/apple-macbook-air-mree2sa-a-i5-8gb-128gb-133-gold-7-2-600x600.jpg";
-import mac2 from "../assets/images.jpg";
+import Dropdown  from 'react-bootstrap/Dropdown';
 
-export default function Bai3() {
+export default function Bai4() {
   return (
-    <div style={{ display: "flex", gap: 20, padding: 20 }}>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={mac1} />
-        <Card.Body>
-          <Card.Title>MacBook Air 2018</Card.Title>
-          <Card.Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur a vitae natus, iure unde consectetur dolor distinctio.
-          </Card.Text>
-          <Button variant="primary">Xem chi tiết</Button>
-          <span style={{ marginLeft: 20 }}>30.000.000 đ</span>
-        </Card.Body>
-      </Card>
+    <Dropdown>
+      <Dropdown.Toggle variant="light" id="dropdown-basic">
+        Nguyen Van Nam
+      </Dropdown.Toggle>
 
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={mac2} />
-        <Card.Body>
-          <Card.Title>MacBook Pro 2019</Card.Title>
-          <Card.Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur a vitae natus, iure unde consectetur dolor distinctio.
-          </Card.Text>
-          <Button variant="primary">Xem chi tiết</Button>
-          <span style={{ marginLeft: 20 }}>30.000.000 đ</span>
-        </Card.Body>
-      </Card>
-    </div>
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Cai dat</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Doi mat khau</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Dang xuat</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 }
